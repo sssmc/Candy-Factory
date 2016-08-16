a1f21ab2160cb0b31df8f3030d15826bec62240b
sssmc <sebastien@robitaille.info>
2016-08-15 23:06:37 -0400

    Initial commit

---
+++ Assets/Scripts/counter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class counter : MonoBehaviour {
+
+	public GameObject candyText;
+	private int candy = 0;
+
+	private void textCounter(){
+		candyText.GetComponent<Text> ().text = "Candy Manufactured: " + candy;
+		candy ++;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("textCounter", 1, 1);
+	
+	}
+}
