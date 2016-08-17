6973080657bf87e88196d5f8dddcb8ef90f7f376
sssmc <sebastien@robitaille.info>
2016-08-17 12:25:00 -0400

    add fonts,temp background image more advanced cady number display

--- Assets/Scripts/counter.cs
+++ Assets/Scripts/counter.cs
@@ -11,8 +11,28 @@ public class counter : MonoBehaviour {
 	public float candy = 0f;
 
 	private void textCounter(){
-		candyText.GetComponent<Text> ().text = "Candy Manufactured: " + Mathf.Round(candy);
+		candyText.GetComponent<Text> ().text = ""+getCandyDisplayNum(candy);
 		candy += candySpeed / 100;
+	}
+
+	private string getCandyDisplayNum(float candy)
+	{
+		float newCandy;
+		string newCandyString;
+
+		if (candy < 10000) {
+			newCandy = Mathf.Round(candy);
+			newCandyString = newCandy.ToString();
+		} else if (candy > 9999 && candy < 1000000) {
+			newCandy = Mathf.Round((candy / 1000)*10) / 10;
+			newCandyString = newCandy.ToString()+" Thousand";
+
+		} 
+		else {
+			newCandy = Mathf.Round(candy);
+			newCandyString = newCandy.ToString();
+		}
+		return newCandyString;
 	}
 
 
@@ -20,15 +40,15 @@ public class counter : MonoBehaviour {
 	void Start () {
 
 		candySpeedText = candySpeedTextObject.GetComponent<Text> ();
-		candySpeedText.text = "Candies per second: " + Mathf.Round (candySpeed);
+		candySpeedText.text = Mathf.Round (candySpeed) + " Per Second ";
 		InvokeRepeating("textCounter", 0.01f, 0.01f);
 	
 	}
 
 	public void changeCandySpeed (float newSpeed){
 
-		candySpeed = newSpeed * 20f;
-		candySpeedText.text = "Candies per second: " + Mathf.Round (candySpeed);
+		candySpeed = newSpeed * 1000f;
+		candySpeedText.text = Mathf.Round (candySpeed) + " Per Second ";
 
 	}
 }
