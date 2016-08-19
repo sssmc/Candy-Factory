8237ee3db9b24d755cead303af17b00835339020
sssmc <sebastien@robitaille.info>
2016-08-18 23:08:46 -0400

    add more complex multi factory support

--- Assets/Scripts/factories.cs
+++ Assets/Scripts/factories.cs
@@ -6,37 +6,72 @@ using UnityEngine.UI;
 public class factories : MonoBehaviour {
 
 	List<Factory> Factories = new List<Factory>();
+	List<GameObject> factoryButtons = new List<GameObject> ();
 	public int currentFactory = 0;
 	public GameObject factoryNameTextObject;
 	public GameObject factroyButtonPrefab;
 	public GameObject factoriesPanel;
-	public GameObject factoryButton;
+	public GameObject canvas;
+	public GameObject factoryPanelPrefab;
 
 	// Use this for initialization
 	void Start () {
-		Factories.Add(new Factory("Default"));
+
+		foreach (Transform child in factoriesPanel.transform) 
+		{
+			factoryButtons.Add(child.gameObject);
+		}
+
+		addFactory ("default", 0);
+		addFactory ("new Factory", 1);
+
 		factoryNameTextObject.GetComponent<Text> ().text = Factories [currentFactory].getName ();
 
-		factoryButton = Instantiate (factroyButtonPrefab);
-		factoryButton.transform.parent = factoriesPanel.transform;
-		factoryButton.GetComponent<RectTransform> ().position = new Vector2 (0, 464);
-
-	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void addFactory(string name, int index)
+	{
+		Factories.Add(new Factory(name,factoryButtons[0], canvas, factoryPanelPrefab));
+		Factories [index].panel = Instantiate (factoryPanelPrefab);
+		Factories [index].panel.transform.parent = canvas.transform;
+		Factories [index].panel.GetComponent<RectTransform> ().localPosition = new Vector2(0,-47);
+		Factories [index].panel.GetComponent<RectTransform>().sizeDelta = new Vector2(549,388);
+		Factories [index].activate ();
+		factoryButtons.RemoveAt (0);
+		setCurrentFactory (index);
+	}
+
+	public void setCurrentFactory (int index)
+	{
+		Factories [currentFactory].deactivate ();
+		currentFactory = index;
+		Factories [currentFactory].activate ();
+		factoryNameTextObject.GetComponent<Text> ().text = Factories [currentFactory].getName ();
 	}
 }
 
-public class Factory {
+public class Factory{
 
 	private string name;
-	public Factory(string newName)
+	private GameObject button;
+	public GameObject panel;
+	public Factory(string newName, GameObject newButton, GameObject canvas, GameObject factoryPanelPrefab)
 	{
 		name = newName;
+		button = newButton;
+		button.transform.GetChild (0).GetComponent<Text> ().text = name;
+		button.GetComponent<Button> ().interactable = true;
 	}
+	public void activate()
+	{
+		button.transform.GetChild (0).GetComponent<Text> ().color = Color.black;
+		panel.SetActive(true);
+	}
+	public void deactivate()
+	{
+		panel.SetActive (false);
+	}
+
 	public string getName()
 	{
 		return name;
